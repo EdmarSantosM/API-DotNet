127b14cbdcf91440b4cdff950e67c4215372b004
EdmarSantosM <santosem4@hotmail.com>
2021-08-27 00:18:23 -0300

    Criação  da Controller
    Criação  da Pasta
    Criação da Classe PessoaController.cs
    Adicionado Bibliotecas :
    
    using System.Threading.Tasks;
    using API_DotNet.Data;
    using Microsoft.AspNetCore.Mvc;
    using Microsoft.EntityFrameworkCore;
    
    Incluído Anotations
    
    [Controller]
    [Route("[controller]")]
    
    Criação  de método oi()
    
    Incluido Annotations
    HttpGet
    
    ( Método Get  - Exibir, Listar etc).

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using API_DotNet.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_DotNet.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class PessoaController
+    {
+        [HttpGet("oi")]
+        public string oi()
+        {
+            return "Hello World";
+        }
+    }
+}
