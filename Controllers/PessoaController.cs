0687220ac644e5715d4d7affb2b3d2dbdf04fcf8
EdmarSantosM <santosem4@hotmail.com>
2021-08-28 22:07:05 -0300

    HttpPost
    Criação do método Post
    
    Faz o cadastramento de dados  na tabela
    
    importado
    using API_DotNet.Models;
    
    ControllerBase (herança)

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -2,13 +2,31 @@ using System.Threading.Tasks;
 using API_DotNet.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using API_DotNet.Models;
 
 namespace API_DotNet.Controllers
 {
     [Controller]
     [Route("[controller]")]
-    public class PessoaController
+    public class PessoaController : ControllerBase
     {
+        private DataContext dc; /* Acesso  as  funçoes  Create, Update, Delete etc */
+        public PessoaController(DataContext context)
+        {
+            this.dc = context;
+        }
+
+        [HttpPost("api")]
+        
+        /* método  de cadastro */
+        public async Task<ActionResult> cadastrar([FromBody]Pessoa p)
+        {
+            dc.pessoa.Add(p);
+            await dc.SaveChangesAsync();
+
+            return Created("Objeto pessoa", p);
+        }
+
         [HttpGet("oi")]
         public string oi()
         {
